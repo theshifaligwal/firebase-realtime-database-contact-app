407269593a40cf39288be30fadd4377e25b05696
theshifaligwal <gwal.shifali2223@gmail.com>
2021-08-10 12:35:54 +0530

    Update files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useEffect } from "react";
 
-function App() {
+import { Container, Col, Row } from "reactstrap";
+
+// react-router-dom3
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+// react toastify stuffs
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+// bootstrap css
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+// firebase stuffs
+//TODO: import firebase config and firebase database
+
+// components
+import AddContact from "./pages/AddContact";
+import Contacts from "./pages/Contacts";
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
+import ViewContact from "./pages/ViewContact";
+import PageNotFound from "./pages/PageNotFound";
+
+// context api stuffs
+//TODO: import reducers and contexts
+
+//initlizeing firebase app with the firebase config which are in ./utils/firebaseConfig
+//TODO: initialize FIREBASE
+
+// first state to provide in react reducer
+const initialState = {
+  contacts: [],
+  contact: {},
+  contactToUpdate: null,
+  contactToUpdateKey: null,
+  isLoading: false
+};
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // will get contacts from firebase and set it on state contacts array
+  const getContacts = async () => {
+    // TODO: load existing data
+  };
+
+  // getting contact  when component did mount
+  useEffect(() => {
+    //FIXME: call methods if needed
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/* FIXME: Provider is not configured */}
+      <ContactContext.Provider>
+        <ToastContainer />
+        <Header />
+        <Container>
+          <Switch>
+            <Route exact path="/contact/add" component={AddContact} />
+            <Route exact path="/contact/view" component={ViewContact} />
+            <Route exact path="/" component={Contacts} />
+            <Route exact path="*" component={PageNotFound} />
+          </Switch>
+        </Container>
+
+        <Footer />
+      </ContactContext.Provider>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/context/Context.js
@@ -0,0 +1,5 @@
+//TODO: Create context: ContactContext
+
+import {createContext} from "react"
+
+export const ContactContext = createContext()
